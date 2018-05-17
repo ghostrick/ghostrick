d2397b9320ff4dd4c3d6b965a86fc31d9fae2dbe
Ghostrick <ghostrick.kinako@gmail.com>
2018-05-17 14:15:56 +0900

    🐰 publish

--- src/index.js
+++ src/index.js
@@ -1,14 +1,100 @@
+#!/usr/bin/env node
+
 const { h, render, Component, Color } = require('ink')
+const QuickSearch = require('ink-quicksearch')
+const opn = require('opn')
+const termImg = require('term-img')
 
-class App extends Component {
-	constructor(){
-		super()
-		this.state = { message: "Hello" }
-	}
+const selectHandler = item => {
 
-	render(){
-		return <Color blue> {this.state.message} </Color>
-	}
+  if(item.url) {
+    opn(item.url, {wait: false})
+  }
+
+  if(item.action) {
+    return item.action()
+  }
+
 }
 
+const indicatorComponent = ({isSelected}) => {
+  return isSelected ? '🐰 ' : '   '
+}
+
+const itemComponent = ({isSelected, children}) => {
+  const color = isSelected ? '#7fbd95' : '#ddd'
+  return <Color hex={color}>{children}</Color>
+}
+
+const selectItems = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/leafia78'
+  },
+  {
+    label: 'GitHub',
+    url: 'https://github.com/Ghostrick'
+  },
+  {
+    label: 'Web',
+    url: 'https://phimos.is'
+  },
+  {
+    label: 'Medium',
+    url: 'https://medium.com/@leafia78/'
+  },
+  {
+    label: 'Hatena Blog',
+    url: 'http://www.ngmrlog.love/'
+  },
+  {
+    label: 'Homete',
+    url: 'https://www.mottohomete.net/leafia78'
+  },
+  {
+    label: 'Exit',
+    action: process.exit
+  }
+]
+
+
+const App = () => {
+
+	return (
+    <div>
+      <div>
+        My nickname is "nagimaru". <br/>
+        I'am web engineer, And web designer.<br/>
+
+        I usually use
+        <Color hex='#7fbd95'> Elixir </Color>
+        and
+        <Color hex='#7fbd95'> Angular </Color>
+        in my work.<br/>
+
+        And, I can develop mobile app with
+        <Color hex='#7fbd95'> ReactNative </Color>
+        and
+        <Color hex='#7fbd95'> MobX</Color>.
+      </div>
+
+      <br/>
+
+      <QuickSearch
+        items={selectItems}
+        onSelect={selectHandler}
+        itemComponent={itemComponent}
+        indicatorComponent={indicatorComponent}
+      />
+    </div>
+  )
+}
+
+const imageFallback = () => {
+  console.log()
+  console.log('(Sorry, Images can not be displayed. Your device is not supported...)')
+  console.log()
+}
+
+termImg(__dirname + '/assets/user.png', {width: '400px', fallback: imageFallback})
 render(<App/>)
